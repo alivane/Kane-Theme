dd1d5f04c55ab43deccd0b1ad93c75a331c4f0b8
alivane <vaneg643@gmail.com>
2016-05-12 09:58:21 -0400

    insertado de navbar a webside

---
+++ css/main.cs
@@ -0,0 +1,30 @@
+/*
+Sass::SyntaxError: "nav" failed to @extend ".z-depth-1".
+The selector ".z-depth-1" was not found.
+Use "@extend .z-depth-1 !optional" if the extend should be able to fail.
+
+
+Backtrace:
+materialize-src/sass/components/_navbar.scss:3
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/tree/visitors/extend.rb:62:in `check_extends_fired!'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/util/subset_map.rb:105:in `each_value'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/util/subset_map.rb:105:in `each'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/util/subset_map.rb:105:in `each_value'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/tree/visitors/extend.rb:51:in `check_extends_fired!'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/tree/visitors/extend.rb:16:in `visit'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/tree/root_node.rb:39:in `css_tree'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/tree/root_node.rb:29:in `render_with_sourcemap'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/engine.rb:381:in `_render_with_sourcemap'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/engine.rb:298:in `render_with_sourcemap'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/exec/sass_scss.rb:391:in `run'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/exec/sass_scss.rb:63:in `process_result'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/exec/base.rb:52:in `parse'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/../lib/sass/exec/base.rb:19:in `parse!'
+/Library/Ruby/Gems/1.8/gems/sass-3.4.22/bin/sass:13
+/usr/bin/sass:19:in `load'
+/usr/bin/sass:19
+*/
+body:before {
+  white-space: pre;
+  font-family: monospace;
+  content: "Sass::SyntaxError: \"nav\" failed to @extend \".z-depth-1\".\A The selector \".z-depth-1\" was not found.\A Use \"@extend .z-depth-1 !optional\" if the extend should be able to fail.\A "; }
